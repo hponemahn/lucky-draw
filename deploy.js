8460021c86a0adf0a15393e476e1432c8bc9d4ab
hponemahn <hponemahn@gmail.com>
2022-11-08 11:09:50 +0630

    done

--- deploy.js
+++ deploy.js
@@ -1,27 +1,33 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
-const {abi, evm} = require("./compile");
-const provider = new HDWalletProvider(
-    
-);
+const { abi, evm } = require("./compile");
+
+const mnemonicPhrase = ""; // 12 word mnemonic
+let provider = new HDWalletProvider({
+  mnemonic: {
+    phrase: mnemonicPhrase,
+  },
+  providerOrUrl: "",
+});
 
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts();
-    console.log("accounts", accounts);
+  const accounts = await web3.eth.getAccounts();
+  console.log("accounts", accounts);
 
-    const result = await new web3.eth.Contract(abi)
+  const result = await new web3.eth.Contract(abi)
     .deploy({
-        data: evm.bytecode.object
-    }).send({
-        from: accounts[0],
-        gas: '1000000'
+      data: evm.bytecode.object,
+    })
+    .send({
+      from: accounts[0],
+      gas: "1000000",
     });
 
-    console.log(JSON.stringify(abi));
-    console.log("Contract deployed to", result.options.address);
-    provider.engine.stop();
-}
+  console.log(JSON.stringify(abi));
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
 
 deploy();
